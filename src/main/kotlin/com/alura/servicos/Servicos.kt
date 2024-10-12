96b4954b817bbb2618951863263cd79a1a2e6140
Arthur-Nevess <git@github.com:Arthur-Nevess/AluGames.git>
2024-10-12 11:51:38 -0300

    Adicionando o requidição de serviços web

--- src/main/kotlin/com/alura/servicos/Servicos.kt
+++ src/main/kotlin/com/alura/servicos/Servicos.kt
@@ -1,5 +1,19 @@
 package com.alura.servicos
 
-class Servicos {
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers
 
+
+class Servicos {
+    var client: HttpClient = HttpClient.newHttpClient()
+
+    var request: HttpRequest = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=612"))
+        .build()
+    var response: HttpResponse<String> = client
+        .send(request, BodyHandlers.ofString())
+    val json = response.body()
 }
