75d2ba415746b40dee41ca96d4fd73694d2f7c4c
Arthur-Nevess <git@github.com:Arthur-Nevess/AluGames.git>
2024-11-14 20:30:07 -0300

    Tornando a class plano abstrata

--- src/main/kotlin/com/alura/Aluguel/Plano.kt
+++ src/main/kotlin/com/alura/Aluguel/Plano.kt
@@ -1,6 +1,6 @@
 package com.alura.Aluguel
 
-open class Plano(tipo: String){
+abstract class Plano(tipo: String){
    open fun calcula (aluguel: Aluguel):Double{
         return aluguel.jogo.preco * aluguel.periodo.emDias
     }
